644ead5e8547e13167927aace36fdd93cbba7ede
Kirill Suslov <euchekavelo@gmail.com>
2025-02-10 08:14:20 +0300

    The model structure for the database and an example JSON file have been updated.

--- src/main/java/ru/skillbox/statisticsservice/dto/response/EventResponseDto.java
+++ src/main/java/ru/skillbox/statisticsservice/dto/response/EventResponseDto.java
@@ -1,23 +1,17 @@
 package ru.skillbox.statisticsservice.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.data.annotation.Id;
+import lombok.Data;
 import ru.skillbox.statisticsservice.model.enums.ObjectType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
 public class EventResponseDto {
 
-    @Id
     private UUID uuid;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateTime;
-
     private ObjectType objectType;
-
     private UUID objectId;
-
     private UUID initiatorUserId;
 }
